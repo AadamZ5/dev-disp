9c5f375597611cde512d5342a50c303e5774a06b
Aadam Zocolo <azocolo@gmail.com>
2025-11-22 15:52:27 -0600

    Add NX to workspace 🥴

--- apps/dev_disp_client/integration_test/simple_test.dart
+++ apps/dev_disp_client/integration_test/simple_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_boundgen/main.dart';
-import 'package:flutter_boundgen/src/rust/frb_generated.dart';
+import 'package:dev_disp_client/main.dart';
+import 'package:dev_disp_client/src/rust/frb_generated.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
